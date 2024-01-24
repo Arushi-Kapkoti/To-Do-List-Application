fe14797cce368744111744d41914a5544c5a0ec8
Nia <114429172+Arushi-Kapkoti@users.noreply.github.com>
2024-01-24 21:58:22 +0530

    Login

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from . views import TaskList, TaskDetail, TaskCreate, TaskUpdate,DeleteView
+from . views import TaskList, TaskDetail, TaskCreate, TaskUpdate,DeleteView, CustomLoginView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LogoutView
 
 urlpatterns=[
+    path('login/',CustomLoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(http_method_names=['get', 'post']),name='logout'),
+    #path('logout/',logout_then_login(http_method_names=['get']),name='logout'),
     path('', TaskList.as_view(),name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(),name='task'),
     path('task-create/', TaskCreate.as_view(),name='task-create'),
     path('taks-update/<int:pk>/',TaskUpdate.as_view(),name='task-update'),
     path('taks-delete/<int:pk>/',DeleteView.as_view(),name='task-delete'),
+
 ]
+
+#path('logout/',LogoutView.as_view(http_method_names=['get', 'post', 'put', 'patch', 'delete', 'head', 'options', 'trace'], next_page='login', redirect_field_name='next'),name='logout'),
--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -3,10 +3,23 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+#for redirection
+
+from django.contrib.auth.views import LoginView, logout_then_login, LogoutView
+
 from .models import Task
 #from django.http import HttpResponse ->we don't need this for class based views
 
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
